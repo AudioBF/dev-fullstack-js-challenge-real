20dc58b7e9df699effd7a301ac8fd266fcca9461
AudioBF <audiobf@gmail.com>
2023-08-21 11:37:42 -0300

    Criando projeto node, instalando o express, nodemon e endpoint para listar e buscar estudantes

---
+++ backend/src/database.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    nome: "Sala undes",
+    email: "devaudio@mal.com",
+    cpf: "789.456.123-55",
+    ra: "1597"
+  },
+  {
+    nome: "Gamora Fags",
+    email: "gamora@mal.com",
+    cpf: "056.456.123-55",
+    ra: "3578"
+  },
+  {
+    nome: "Salem Tdes",
+    email: "salem@mal.com",
+    cpf: "123.456.123-55",
+    ra: "3654"
+  },
+];
