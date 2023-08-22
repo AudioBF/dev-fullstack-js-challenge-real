bcb3a985308b63806d219b9d2f2e82c4196d52a1
AudioBF <audiobf@gmail.com>
2023-08-22 20:46:30 -0300

    Importando jQuery na lista de estudantes, fazendo uma requisição get no endpoint de / users e instalando o cors no backend para a aceitar a requisição e listando usuários na UI

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,20 +1,32 @@
 module.exports = [
   {
-    nome: "Sala undes",
+    nome: "Aluno 1",
     email: "devaudio@mal.com",
     cpf: "789.456.123-55",
-    ra: "1597"
+    ra: "1234"
   },
   {
-    nome: "Gamora Fags",
+    nome: "Aluno 2",
     email: "gamora@mal.com",
     cpf: "056.456.123-55",
-    ra: "3578"
+    ra: "1235"
   },
   {
-    nome: "Salem Tdes",
+    nome: "Aluno 3",
     email: "salem@mal.com",
     cpf: "123.456.123-55",
-    ra: "3654"
+    ra: "1236"
+  },
+  {
+    nome: "Aluno 4",
+    email: "sdaasc@mal.com",
+    cpf: "999.456.123-55",
+    ra: "1237"
+  },
+  {
+    nome: "Aluno 5",
+    email: "sadaslem@mal.com",
+    cpf: "013.456.123-55",
+    ra: "1238"
   },
 ];
