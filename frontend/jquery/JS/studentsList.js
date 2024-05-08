d633077bb4568df35b7276b9d5ad5223baee4110
AudioBF <audiobf@gmail.com>
2024-05-08 19:28:33 -0300

    Alterado a porta da API para 3006 | Criado component StudentListPage e feito o Fetch na lista de estudantes na API

--- frontend/jquery/JS/studentsList.js
+++ frontend/jquery/JS/studentsList.js
@@ -28,32 +28,4 @@ const deleteStudent = (ra) => {
     });
 };
 
-function fetchStudentList(searchQuery = "") {
-  $(".loader").show("fast");
-  $(".content-page").hide();
 
-  fetch(`http://localhost:3000/students/list/${searchQuery}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const table = $("#studentList tbody");
-      table.html("");
-      data.map((student) => {
-        table.append(`
-            <tr>
-                <td>${student.ra}</td>
-                <td>${student.name}</td>
-                <td>${student.cpf}</td>
-                <td>
-                    <a href="studentManager.html?ra=${student.ra}">Editar</a>
-                    <a class="removeStudent" data-ra="${student.ra}" href="#">Excluir</a>
-                </td>
-            </tr>
-        `);
-      });
-
-      $(".loader").hide("fast");
-      $(".content-page").show("slow");
-    });
-}
--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,12 +1,61 @@
 import "./App.css";
+import Navbar from "./componets/shared/Navbar";
+import StudentListPage from "./componets/pages/StudentList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     <h1>HELLO AUDIO</h1>
+    <div className="main-container">
+      <Navbar />
+      <section className="container">
+        <header className="main-header">Consulta de Alunos</header>
+        <div className="loader"></div>
+        <div className="content-page">
+         <StudentListPage/> 
+        </div>
+      </section>
 
-      </header>
+
+      <nav className="main-nav">
+        <header>Modulo Academico</header>
+        <ul className="nav-links">
+          <a className="nav-item" href="studentsList.html">
+            <li>Alunos</li>
+          </a>
+        </ul>
+      </nav>
+      <section className="container">
+        <header className="main-header">Cadastro de Aluno</header>
+        <div className="loader"></div>
+        <div className="content-page padding-lr-20">
+          <form id="studentForm" className="form" action="" method="post">
+            <div className="form-group">
+              <label htmlFor="name">Nome</label>
+              <input required type="text" name="name" id="name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input required type="text" name="email" id="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="ra">RA</label>
+              <input required type="number" name="ra" id="ra" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="cpf">CPF</label>
+              <input required type="number" name="cpf" id="cpf" />
+            </div>
+            <div className="actions">
+              <a
+                href="studentsList.html"
+                className="btn btn-warning margin-right-10"
+              >
+                Cancelar
+              </a>
+              <button className="btn">Salvar</button>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
